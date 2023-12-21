83e4452b1d961f23d0bc54484e38e3301f262037
Buddi Navaneeth Kumar <35196622+Nava308@users.noreply.github.com>
2023-12-20 20:47:33 -0600

    countVowels assign

--- 01-js/medium/countVowels.js
+++ 01-js/medium/countVowels.js
@@ -4,9 +4,17 @@
 
   Once you've implemented the logic, test your code by running
 */
-
+const isVowel = (char) =>
+  char === "a" || char === "e" || char === "i" || char === "o" || char === "u";
 function countVowels(str) {
-    // Your code here
+  const string = str.toLowerCase();
+  var vowelCount = 0;
+  for (var char of string) {
+    if (isVowel(char)) {
+      vowelCount++;
+    }
+  }
+  return vowelCount;
 }
 
 module.exports = countVowels;
