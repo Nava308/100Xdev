d8ca583029464652d6b464e9ee50546f0df683d1
Buddi Navaneeth Kumar <35196622+Nava308@users.noreply.github.com>
2023-12-20 20:42:13 -0600

    palindrome assign

--- 01-js/medium/palindrome.js
+++ 01-js/medium/palindrome.js
@@ -3,7 +3,17 @@
   Note: the input string is case-insensitive which means 'Nan' is a palindrom as 'N' and 'n' are considered case-insensitive.
 */
 
-function isPalindrome(str) {
+function isPalindrome(string) {
+  let str = string.replace(/[^\w]/g, "");
+  var start = 0;
+  var end = str.length - 1;
+  while (start < end) {
+    if (str[start].toLowerCase() !== str[end].toLowerCase()) {
+      return false;
+    }
+    start++;
+    end--;
+  }
   return true;
 }
 
