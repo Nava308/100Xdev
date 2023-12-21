339da31a7469cacfd150093eb20b470ab5b82fad
Buddi Navaneeth Kumar <35196622+Nava308@users.noreply.github.com>
2023-12-20 22:01:52 -0600

    calculator assign

--- 01-js/hard/calculator.js
+++ 01-js/hard/calculator.js
@@ -15,7 +15,136 @@
 
   Once you've implemented the logic, test your code by running
 */
+const isValidNumber = (input) => {
+  if (Number.isNaN(Number(input))) {
+    throw new Error("Invalid number");
+  }
+  return true;
+};
+class Calculator {
+  constructor() {
+    this.result = 0;
+    this.precedence = {
+      "+": 1,
+      "-": 1,
+      "*": 2,
+      "/": 2,
+    };
+  }
+  add(num) {
+    try {
+      if (isValidNumber(num)) {
+        this.result = this.result + num;
+      }
+    } catch (error) {
+      throw error;
+    }
+  }
+  subtract(num) {
+    try {
+      if (isValidNumber(num)) {
+        this.result = this.result - num;
+      }
+    } catch (error) {
+      throw error;
+    }
+  }
+  multiply(num) {
+    try {
+      if (isValidNumber(num)) {
+        this.result = this.result * num;
+      }
+    } catch (error) {
+      throw error;
+    }
+  }
+  divide(num) {
+    try {
+      if (num === 0) {
+        throw new Error("divide by zero error");
+      }
+      if (isValidNumber(num)) {
+        this.result = this.result / num;
+      }
+    } catch (error) {
+      throw error;
+    }
+  }
+  clear() {
+    this.result = 0;
+  }
+  getResult() {
+    return this.result;
+  }
+  applyOperator(values, operator) {
+    const b = values.pop();
+    const a = values.pop();
+    switch (operator) {
+      case "+":
+        values.push(a + b);
+        break;
+      case "-":
+        values.push(a - b);
+        break;
+      case "*":
+        values.push(a * b);
+        break;
+      case "/":
+        if (b === 0) throw new Error("divide by 0 error");
+        values.push(a / b);
+        break;
+      default:
+        throw new Error(`Invalid operator: ${operator}`);
+    }
+  }
+  evaluate(tokens) {
+    const values = [];
+    const operators = [];
 
-class Calculator {}
+    for (const token of tokens) {
+      if (/\d+/.test(token)) {
+        values.push(parseFloat(token));
+      } else if (token === "(") {
+        operators.push(token);
+      } else if (token === ")") {
+        while (operators.length && operators[operators.length - 1] !== "(") {
+          this.applyOperator(values, operators.pop());
+        }
+        operators.pop(); // Pop '('
+      } else {
+        while (
+          operators.length &&
+          this.precedence[operators[operators.length - 1]] >=
+            this.precedence[token]
+        ) {
+          this.applyOperator(values, operators.pop());
+        }
+        operators.push(token);
+      }
+    }
 
+    while (operators.length) {
+      this.applyOperator(values, operators.pop());
+    }
+
+    this.result = values[0];
+  }
+  calculate(string) {
+    try {
+      eval(string);
+      const tokens = string.match(/\d+(\.\d+)?|\+|\-|\*|\/|\(|\)/g);
+
+      if (!tokens) {
+        throw new Error("Invalid characters in the expression.");
+      }
+      console.log(tokens);
+      this.evaluate(tokens);
+    } catch (error) {
+      throw error;
+    }
+  }
+}
+cal = new Calculator();
+cal.calculate("(2.5 + 1.5) * 3");
+console.log(cal.getResult());
 module.exports = Calculator;
