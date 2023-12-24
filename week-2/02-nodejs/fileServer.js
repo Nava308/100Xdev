4b068ec33115ebda8442a94120ba9fed7f69ded7
Buddi Navaneeth Kumar <35196622+Nava308@users.noreply.github.com>
2023-12-24 14:58:28 -0600

    filer server assignment

--- week-2/02-nodejs/fileServer.js
+++ week-2/02-nodejs/fileServer.js
@@ -12,10 +12,49 @@
     - For any other route not defined in the server return 404
     Testing the server - run `npm run test-fileServer` command in terminal
  */
-const express = require('express');
-const fs = require('fs');
-const path = require('path');
+const express = require("express");
+const fs = require("fs");
+const path = require("path");
 const app = express();
 
+const getFilesInFolder = () => {
+  return new Promise((resolve, reject) => {
+    fs.readdir("./files/", (err, files) => {
+      if (err) reject(err);
+      else resolve(files);
+    });
+  });
+};
 
+const getFileContent = (filename) => {
+  return new Promise((resolve, reject) => {
+    fs.readFile(path.join("./files", filename), "utf-8", (error, data) => {
+      if (error) reject(error);
+      else resolve(data);
+    });
+  });
+};
+
+app.get("/files", async (req, res) => {
+  try {
+    const files = await getFilesInFolder();
+    return res.status(200).json(files);
+  } catch (error) {
+    return res.status(500).json(error);
+  }
+});
+app.get("/file/:filename", async (req, res) => {
+  try {
+    const { filename } = req.params;
+    //console.log(filename);
+    const data = await getFileContent(filename);
+    return res.status(200).send(data);
+  } catch (error) {
+    console.log(error);
+    return res.status(404).send("File not found");
+  }
+});
+app.all("*", (req, res) => {
+  return res.status(404).send("Route not found");
+});
 module.exports = app;
